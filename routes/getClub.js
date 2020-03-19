5e230dac1601487d08e7396aa1ff4c65708103f0
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-03-19 12:36:32 +0330

    last changes

--- routes/getClub.js
+++ routes/getClub.js
@@ -23,41 +23,36 @@ export default function getClub(teamId, cb) {
 
     // log('@@@@@@@@@@@@@@@@@@@@@@@@', country, city);
     if (group === 'AFC' && country && city) {
-      fs.readFile(
-        `/home/emad/Documents/projects/FANS_CLUB_LAST/fans_club_front_github/routes/logo/${country +
-          city +
-          name}.png`,
-        (errRead, img) => {
-          if (errRead) {
-            log('---------------------', errRead);
-            return cb(200, { club, errRead });
-          }
-          City.findOne(
-            {
-              country: country.trim(),
-              name: city.trim(),
-            },
-            async (errCity, cityObj) => {
-              if (err) {
-                log('--------------- finding City', err);
-                return cb(200, { club, errCity });
-              }
-              if (!cityObj) {
-                log('--------------- no city has been found', cityObj);
-                return cb(200, { club, errCity });
-              }
-              const base64Image = Buffer.from(img).toString('base64');
-
-              // Buffer.from(img, 'binary').toString('base64');
-
-              log('-----------------', cityObj);
-              return cb(200, { city: cityObj, club, base64Image });
-
-              // TODO: send new json with club, city and buffer
-            }
-          );
+      fs.readFile(`~/repos/fans_club/routes/logo/${country + city + name}.png`, (errRead, img) => {
+        if (errRead) {
+          log('---------------------', errRead);
+          return cb(200, { club, errRead });
         }
-      );
+        City.findOne(
+          {
+            country: country.trim(),
+            name: city.trim(),
+          },
+          async (errCity, cityObj) => {
+            if (err) {
+              log('--------------- finding City', err);
+              return cb(200, { club, errCity });
+            }
+            if (!cityObj) {
+              log('--------------- no city has been found', cityObj);
+              return cb(200, { club, errCity });
+            }
+            const base64Image = Buffer.from(img).toString('base64');
+
+            // Buffer.from(img, 'binary').toString('base64');
+
+            log('-----------------', cityObj);
+            return cb(200, { city: cityObj, club, base64Image });
+
+            // TODO: send new json with club, city and buffer
+          }
+        );
+      });
     } else {
       return cb(200, club);
     }
