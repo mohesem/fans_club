94c9433532dfc9be25024c08a524d8b46b539d55
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-03-26 20:59:13 +0430

    last changes

--- routes/deleteClub.js
+++ routes/deleteClub.js
@@ -20,7 +20,7 @@ export default async function(body, cb) {
       if (error) return cb(500, { other: 'Server Internal Error' });
       if (!user) return cb(400, 'User doesnt exist');
 
-      const newLikeOrDislike = user[type].filter(id => id !== club._id);
+      const newLikeOrDislike = user[`${type}s`].filter(id => id !== club._id);
 
       user[type] = newLikeOrDislike;
 
