bd019513faaddc4555b6d522e3037ea888726ba0
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-03-31 06:57:45 +0430

    lastchanges

--- routes/getLikesForPolys.js
+++ routes/getLikesForPolys.js
@@ -11,7 +11,7 @@ function getLikeNumbers(body) {
   const calculatedFid = fid => {
     if (fid < 1000) return fid;
     if (fid > 1000) return fid + 1000;
-    if (fid > 1000) return fid + 10000;
+    if (fid > 10000) return fid + 10000;
   };
   return new Promise((resolve, reject) => {
     try {
