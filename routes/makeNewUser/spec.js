78e29fc2fb04f9d6b55f92e3dff96621a7367b9b
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-05-31 18:18:24 +0430

    last changes

--- routes/makeNewUser/spec.js
+++ routes/makeNewUser/spec.js
@@ -4,12 +4,26 @@ export default body => {
   const rulesObj = [
     { value: body.from, name: 'from', required: true, type: 'string' },
     { value: body.email, name: 'email', required: true, email: true },
-    { value: body.firstname, name: 'firstname', required: true, type: 'string' },
+    {
+      value: body.firstname,
+      name: 'firstname',
+      required: true,
+      type: 'string',
+    },
     { value: body.lastname, name: 'lastname', required: true, type: 'string' },
     { value: body.fullname, name: 'fullname', required: true, type: 'string' },
-    { value: body.picture, name: 'picture', required: true, type: 'string' },
-    { value: body.thumbnail, name: 'thumbnail', required: true, type: 'string' },
-    { value: body.googleId || body.facebookId, name: 'id', required: true, type: 'string' },
+    {
+      value: body.thumbnail,
+      name: 'thumbnail',
+      required: true,
+      type: 'string',
+    },
+    {
+      value: body.googleId || body.facebookId,
+      name: 'id',
+      required: true,
+      type: 'string',
+    },
     {
       value: body.location,
       name: 'location',
