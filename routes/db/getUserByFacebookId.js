992853fdb6d00ec08d2cf28788d1c7da3abaee5a
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-01 13:19:08 +0430

    last changes

--- routes/db/getUserByFacebookId.js
+++ routes/db/getUserByFacebookId.js
@@ -4,10 +4,10 @@ import User from '../../DB/models/userModel';
 const log = debug('log:db');
 
 export default function getUserByEmail(facebookId, cb) {
-  log('gert user by facebookId is working', facebookId);
+  console.log('gert user by facebookId is working', facebookId);
 
   User.findOne({ facebookId }, (err, user) => {
-    log('getUserByFacebookID result', err, user);
+    console.log('getUserByFacebookID result', err, user);
     if (err) return cb(err, null);
     return cb(null, user);
   });
