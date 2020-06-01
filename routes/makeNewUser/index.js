408d8a1404b66edcaa7045f103e58a22beee78de
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-01 16:31:45 +0430

    last changes

--- routes/makeNewUser/index.js
+++ routes/makeNewUser/index.js
@@ -6,6 +6,7 @@ import makeNewUser from '../db/makeNewUser';
 const log = debug('log:v1');
 
 export default async function(body, cb) {
+  console.log('make ne user is :::', body);
   const test = await spec(body);
   log('test isNewUser', test);
 
