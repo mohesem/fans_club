02c1d7497cc49d110e3e6b424a12fc1f232682c5
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-04 20:05:33 +0430

    last changes

---
+++ react/build/precache-manifest.51559d844c6f64a2ffe01b0b90fda8fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1427e8388d52ef5a9ea66e1eb0901f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f53b7f8adb257c9965aa",
+    "url": "/static/js/2.72943627.chunk.js"
+  },
+  {
+    "revision": "409d29ce1d48c65d2b7514ba71127d2a",
+    "url": "/static/js/2.72943627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6bd46c2af0fd17ad7c",
+    "url": "/static/js/main.a18d24f3.chunk.js"
+  },
+  {
+    "revision": "f2284be62ded930f8c86",
+    "url": "/static/js/runtime-main.e3a6a3cf.js"
+  },
+  {
+    "revision": "8d95f2be197dde115710727bfdfa903b",
+    "url": "/static/media/3.8d95f2be.jpeg"
+  },
+  {
+    "revision": "898e6949ebdf9f1ca71997951c1bd0db",
+    "url": "/static/media/logoandlogotype.898e6949.png"
+  }
+]);
