04690475f816d8446b7ea598e33390a60dfe9cb4
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-16 22:07:27 +0430

    last changes

--- routes/admin/isAdmin.js
+++ routes/admin/isAdmin.js
@@ -2,9 +2,11 @@ import jwt from 'jsonwebtoken';
 import Admin from '../../DB/models/admin';
 
 export default async function(username, password, cb) {
+  console.log('=============', username, password);
   if (!username || !password) return cb(200, { err: true, other: 'bad request' });
 
   Admin.findOne({ username, password }, (err, admin) => {
+    console.log('************ ', err, admin);
     if (err) return cb(200, { err: true, other: 'server internal error' });
     if (!admin) return cb(200, { err: true, other: 'no uwer had been found' });
     jwt.sign({ username, password }, process.env.TOKEN, (error, token) => {
