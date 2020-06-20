d2928a0ecd476a623bb29643e184d3d30eb9f72f
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-20 12:10:28 +0430

    last changes

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.use(express.static(path.resolve('react', 'build')));
 // app.use(cookieParser());
 // app.use(logger('dev'));
 
-app.use(Cors());
+app.use(Cors('*'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
