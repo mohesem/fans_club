1f58101583fc175dd560d7a16529553167372f96
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-20 16:38:18 +0430

    last changes

---
+++ routes/clubs/details.js
@@ -0,0 +1,26 @@
+import Teams from '../../DB/models/teamsModel';
+import Likes from '../../DB/models/likeModel';
+import Dislikes from '../../DB/models/dislikeModel';
+
+const header = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  'Content-Type': 'application/json',
+};
+
+const headerErr = { 'Content-Type': 'text/plain' };
+
+export default async function(teamId, cb) {
+  try {
+    const countLikes = await Likes.count({ team_id: teamId });
+    const countDislikes = await Dislikes.count({ team_id: teamId });
+    console.log({ countDislikes, countLikes });
+  } catch (error) {
+    console.log(error);
+  }
+
+  Teams.countDocuments({}, (err, result) => {
+    if (err) return cb(200, headerErr, { err: true, other: 'internal server error' });
+    return cb(200, header, { err: false, count: result });
+  });
+}
--- routes/clubs/index.js
+++ routes/clubs/index.js
@@ -3,12 +3,24 @@ import { Router } from 'express';
 import isAdmin from '../middlewares/isAdmin';
 
 import count from './count';
+import details from './details';
 
 const router = Router();
 router.get('/count/:token', isAdmin, (req, res) => {
   console.log('got the req');
 
   count((status, header, trace) => {
+    res
+      .set(header)
+      .status(status)
+      .send({ trace });
+  });
+});
+
+router.get('/count/:teamId/:token', isAdmin, (req, res) => {
+  console.log('got the req');
+
+  details((status, header, trace) => {
     res
       .set(header)
       .status(status)
