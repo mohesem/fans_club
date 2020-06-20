719429a5b1e3dfc6c022348df10a193e166d992d
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-06-20 11:49:07 +0430

    last changes

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -9,7 +9,10 @@ const router = Router();
 //   res.status(200).send({ msg: 'got the req bro' });
 // });
 
-router.get('/verify/:username/:password', (req, res) => {
+router.get('/verify/:username/:password', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
   isAdmin((req.params.username, req.params.password), (status, trace) => {
     res.status(status).send(trace);
   });
