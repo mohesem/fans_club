7ca4e3e21b4edb12143012ebffb715463497649c
mohesem <mohesem@gmail.com>
2020-08-25 11:13:38 +0000

    last changes

--- routes/changeUserLocation.js
+++ routes/changeUserLocation.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import User from '../DB/models/userModel';
+import * as turf from '@turf/turf';
 
 const main = body => {
   return new Promise((resolve, reject) => {
@@ -8,8 +9,21 @@ const main = body => {
       const session = await mongoose.startSession();
       session.startTransaction();
       try {
-        jwt.verify(body.token, process.env.TOKEN, (err, decoded) => {
+        jwt.verify(body.token, process.env.TOKEN, async (err, decoded) => {
           console.log(decoded);
+          const user = await User.findById(decoded.id).exec();
+          console.log(user);
+          const point = turf.point([
+            body.UserLocation.location.lng,
+            body.UserLocation.location.lat,
+          ]);
+          user.geo = point.geometry;
+          user.address = body.UserLocation.address;
+          console.log(user);
+          // save user
+
+          // get new geos
+          // populate likes and dislike
         });
         //  const user = await User.findOne({_id === body.User.})
       } catch (error) {
